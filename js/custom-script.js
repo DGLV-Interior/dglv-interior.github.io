e32d79fcf9f3ccbc88a49892b0e4172381d1897f
chinnis <chinni.s@proxo.in>
2025-08-29 08:05:32 +0530

    new loader and social media links updated

--- js/custom-script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  const splash = document.getElementById("splash");
-  const mainContent = document.getElementById("main-content");
-
-  setTimeout(() => {
-    splash.style.display = "none";
-    mainContent.style.display = "block";
-  }, 2500); // 2.5 seconds
-});
